db56eec07ce2b52497d55f870581c9db69ba4292
harryng0312 <little.ruan@gmail.com>
2023-01-11 14:07:31 +0700

    try by opencl

--- src/main/basic/oop/Decorator.py
+++ src/main/basic/oop/Decorator.py
@@ -1,11 +1,18 @@
-def my_decorator(func):
-    def wrapper(name:str, *args, **kwargs):
-        print("Something is happening before the function is called.")
-        func(name)
-        print("Something is happening after the function is called.")
-    return wrapper
+from functools import wraps
 
-@my_decorator
+
+def wrap_dec(loopcount: int):
+    def my_decorator(func):
+        # @wraps(func)
+        def wrapper(name: str, *args, **kwargs):
+            print("Something is happening before the function is called.")
+            for i in range(0, loopcount):
+                func(name)
+            print("Something is happening after the function is called.")
+        return wrapper
+    return my_decorator
+
+@wrap_dec(loopcount=4)
 def say_whee(name:str):
     print(f"Whee! {name}")
 
