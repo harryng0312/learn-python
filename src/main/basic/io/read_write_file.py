50c22a20250b536acfdff7efef612f504ed11424
harryng0312 <little.ruan@gmail.com>
2023-02-13 11:34:47 +0700

    copy file

---
+++ src/main/basic/io/read_write_file.py
@@ -0,0 +1,27 @@
+
+from io import FileIO
+
+
+FILE_NAME: str = "data/matrix/mat1.txt"
+FILE_NAME_2: str = "data/matrix/mat1_copy.txt"
+
+def copy_file(srcFilePath: str, destFilePath: str):
+    srcFile: FileIO
+    destFile: FileIO
+    buffSize: int = 1_024
+    buffer: bytes = None
+    try:
+        with open(file = srcFilePath, mode = "+rb", buffering = buffSize) as srcFile:
+            with open(file = destFilePath, mode = "+bw", buffering = buffSize) as destFile:
+                buffer = srcFile.read(buffSize)
+                while(buffer):
+                    destFile.write(buffer)
+                    destFile.flush()
+                    buffer = srcFile.read(buffSize)
+    except Exception as ex:
+        print(f"Exception: {ex}")
+    finally:
+        pass
+
+print("printing...")
+copy_file(FILE_NAME, FILE_NAME_2)
