0ac3ea2f8822abdb54395c7f94e215f844e31d50
harryng0312 <little.ruan@gmail.com>
2023-02-14 12:12:29 +0700

    threads

--- src/main/basic/threads/thread_basic.py
+++ src/main/basic/threads/thread_basic.py
@@ -1,3 +1,4 @@
+
 # import thread
 import datetime
 # import os
@@ -5,12 +6,13 @@ import datetime
 # current = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 # sys.path.append(current)
 # from util import logger_conf as log
-# import os
-# import sys
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../', '../')))
-from . import thread_basic
-from util import logger_conf as log
+import os
+import sys
+# print(f"before syspath:{sys.path}")
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../', '../')))
+# print(f"after syspath:{sys.path}")
+from util.logger_conf import logger
 
 nowStr: str = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-log.logger.info(f"datetime now:{nowStr}")
+logger.info(f"datetime now:{nowStr}")
 # print(f"__name__:{__name__}")
