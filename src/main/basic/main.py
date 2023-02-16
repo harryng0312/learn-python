13f5883c4701bc4bb14417fb817e38ba7decfb78
harrynguyen0312 <Hiepxie1987>
2023-02-16 23:47:02 +0700

    threadpool

--- src/main/basic/main.py
+++ src/main/basic/main.py
@@ -10,6 +10,13 @@ from learn_math import matrix as mt
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    arr1: (float) = (1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
+    arr2: list[float] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+    arr3: (float) = arr1
+    arr4: list[float] = arr2
+    a: float = 3.0
+    print(f"size of int:{sys.getsizeof(a)} type:{type(a)} \narr1:{id(arr1)} arr2:{id(arr2)}\n" 
+        + f"arr3:{id(arr3)} arr4:{id(arr4)}")
     mt.print_matrix()
     print(f"Kích thước số: {sys.getsizeof(np.int8(6.0))}")
 
