8952738a1945ae4b1d3e3e38b1922e0b396dfd3b
harryng0312 <little.ruan@gmail.com>
2023-02-17 10:34:00 +0700

    threads

--- src/main/basic/io/read_write_file.py
+++ src/main/basic/io/read_write_file.py
@@ -11,17 +11,18 @@ def copy_file(srcFilePath: str, destFilePath: str):
     buffSize: int = 1_024
     buffer: bytes = None
     try:
-        with open(file = srcFilePath, mode = "+rb", buffering = buffSize) as srcFile:
-            with open(file = destFilePath, mode = "+bw", buffering = buffSize) as destFile:
+        with open(file = srcFilePath, mode = "+rb", buffering = buffSize) as srcFile, \
+            open(file = destFilePath, mode = "+bw", buffering = buffSize) as destFile:
+            # with open(file = destFilePath, mode = "+bw", buffering = buffSize) as destFile:
+            buffer = srcFile.read(buffSize)
+            while(buffer):
+                destFile.write(buffer)
+                destFile.flush()
                 buffer = srcFile.read(buffSize)
-                while(buffer):
-                    destFile.write(buffer)
-                    destFile.flush()
-                    buffer = srcFile.read(buffSize)
     except Exception as ex:
         print(f"Exception: {ex}")
     finally:
         pass
 
-print("printing...")
+print("copying...")
 copy_file(FILE_NAME, FILE_NAME_2)
