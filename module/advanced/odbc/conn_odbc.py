6d4ccd5ee4062c96856117c626830f0ffff27326
harryng0312 <little.ruan@gmail.com>
2023-03-23 01:39:43 +0700

    db connect select

---
+++ module/advanced/odbc/conn_odbc.py
@@ -0,0 +1,52 @@
+import contextlib
+
+from module.util.logger_conf import logger
+
+import pyodbc
+
+DB_HOST = "."
+DB_PORT = 1433
+DB_NAME = "test_db"
+DB_UNAME = "sa"
+DB_PASSWD = "123456"
+
+
+@contextlib.contextmanager
+def create_conn() -> pyodbc.Connection:
+    logger.info(f"openning connection")
+    # conn = pyodbc.connect("DRIVER={SQL Server};"
+    #                       f"Server={DB_HOST};Port={DB_PORT};"
+    #                       f"Database={DB_NAME};User ID={DB_UNAME};Password={DB_PASSWD}")
+    conn = pyodbc.connect("DRIVER={SQL Server};",
+                          server=DB_HOST, port=DB_PORT,
+                          database=DB_NAME, user_id=DB_UNAME, password=DB_PASSWD)
+    logger.info(f"openned connection")
+    conn.autocommit = False
+    try:
+        yield conn
+        pass
+    finally:
+        close_conn(conn)
+
+
+def close_conn(conn: pyodbc.Connection) -> None:
+    try:
+        if not conn.autocommit and not conn.closed:
+            conn.commit()
+            logger.info(f"commit connection")
+            pass
+        pass
+    except Exception as ex:
+        if not conn.closed:
+            conn.rollback()
+            logger.info(f"rollback connection")
+            pass
+        raise ex
+        pass
+    finally:
+        if not conn.closed:
+            conn.close()
+            logger.info(f"closed connection")
+            pass
+        pass
+    pass
---
+++ module/advanced/odbc/select_odbc.py
@@ -0,0 +1,60 @@
+import pyodbc
+
+from module.advanced.odbc.conn_odbc import create_conn, close_conn
+from module.util.logger_conf import logger
+
+
+def select_one() -> None:
+    sql: str = """
+    SELECT customer_id
+      ,cust_name
+      ,city
+      ,grade
+      ,salesman_id
+  FROM customer
+  WHERE customer_id = ?
+    """
+    with create_conn() as conn:
+        cursor: pyodbc.Cursor = conn.cursor()
+        cursor.execute(sql, 1)
+        # cursor.execute(sql, 3001)
+        colums: [str] = [x[0] for x in cursor.description]
+        row: pyodbc.Row = cursor.fetchone()
+        if row:
+            row_dict: dict = dict(zip(colums, row))
+            # logger.info(f"id: {row.customer_id} name: {row[1]}")
+            logger.info(f"id: {row_dict['customer_id']} name: {row_dict['cust_name']}")
+            pass
+        pass
+    pass
+
+
+def select_all() -> None:
+    sql: str = """
+        SELECT customer_id
+          ,cust_name
+          ,city
+          ,grade
+          ,salesman_id
+      FROM customer
+      WHERE city = ?
+        """
+    with create_conn() as conn:
+        cursor: pyodbc.Cursor = conn.cursor()
+        cursor.execute(sql, "New York")
+        # cursor: pyodbc.Cursor = conn.execute(sql, "New York")
+        colums: [str] = [x[0] for x in cursor.description]
+        rows: pyodbc.Row = cursor.fetchall()
+        # row_dict: dict = dict(zip(colums, row))
+        for row in rows:
+            row_dict: dict = dict(zip(colums, row))
+            # logger.info(f"id: {row.customer_id} name: {row[1]}")
+            logger.info(f"id: {row_dict['customer_id']} name: {row_dict['cust_name']}")
+            pass
+        pass
+    # close_conn(conn)
+    pass
+
+
+select_one()
+select_all()
