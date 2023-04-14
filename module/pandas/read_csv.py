66626f15da8285580ecc140292819e78389a6985
harryng0312 <little.ruan@gmail.com>
2023-04-14 09:44:22 +0700

    pandas

--- module/pandas/read_csv.py
+++ module/pandas/read_csv.py
@@ -14,8 +14,10 @@ def load_csv() -> pd.DataFrame:
 
 
 def select_data(data_frm: pd.DataFrame) -> None:
-    data_frm['OrderDateKey'] = pd.to_datetime(arg=data_frm['OrderDateKey'], format=r"%Y/%m/%d")
-    filter_result: np.ndarray = data_frm[(data_frm['ProductKey'] > 600) | (data_frm['OrderDateKey'] > dt.strptime("2021/02/01", r"%Y/%m/%d"))]
+    data_frm['OrderDateKey'] = pd.to_datetime(arg=data_frm['OrderDateKey'], format=r"%Y%m%d")
+    data_frm['DueDateKey'] = pd.to_datetime(arg=data_frm['DueDateKey'], format=r"%Y%m%d")
+    data_frm['ShipDateKey'] = pd.to_datetime(arg=data_frm['ShipDateKey'], format=r"%Y%m%d")
+    filter_result: np.ndarray = data_frm[(data_frm['ProductKey'] > 600) & (data_frm['OrderDateKey'] > dt.strptime("2022/06/01", r"%Y/%m/%d"))]
     logger.info(f"{filter_result}")
     return
 
