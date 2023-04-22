9f331e11a373536b4c9631026b1ac5a4a3e59a32
harrynguyen0312 <Hiepxie1987>
2023-04-22 09:50:43 +0700

    sympy

---
+++ module/sympy/sympy_basic.py
@@ -0,0 +1,29 @@
+import sympy as sp
+
+from module.util.logger_conf import logger
+
+
+def find_limit() -> None:
+    x: sp.Symbol = sp.symbols("x");
+    logger.info(f"x = {x}")
+    lim = sp.limit(sp.sin(x), x, 0, dir="-")
+    logger.info(f"lim:{lim}")
+    return
+
+
+def find_derivate() -> None:
+    return
+
+def find_integrate() -> None:
+    x = sp.symbols('x') 
+    expr = x**2 + x + 1 
+    integrate = sp.integrate(expr, x)
+    # sp.init_session()
+    # logger.info(f"integrate:{integrate}")
+    # sp.pprint(expr=expr)
+    return
+
+if __name__ == "__main__":
+    # sp.init_printing()
+    find_integrate()
+    pass
