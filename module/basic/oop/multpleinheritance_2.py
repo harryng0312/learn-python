61018f86c73c5177a7825653a6fe5563d87d0a80
harryng0312 <little.ruan@gmail.com>
2023-06-06 10:18:28 +0700

    core

--- module/basic/oop/multpleinheritance_2.py
+++ module/basic/oop/multpleinheritance_2.py
@@ -1,14 +1,12 @@
-class A(object):
+from abc import ABC, abstractmethod
+import typing as tp
+
+
+class A(ABC):
     def __init__(self):
         print("-> A")
         super(A, self).__init__()
         print("<- A")
-
-class B(A):
-    def __init__(self):
-        print("-> B")
-        super(B, self).__init__()
-        print("<- B")
 
 class C(A):
     def __init__(self):
@@ -17,12 +15,27 @@ class C(A):
         super(C, self).__init__()
         print("<- C")
 
-class D(B,C):
+class B(A):
+    def __init__(self):
+        print("-> B")
+        super(B, self).__init__()
+        print("<- B")
+    
+    @abstractmethod
+    def get_abstract_value():
+        pass
+
+
+class D(B, C):
     def __init__(self):
         print("-> D")
         # Use super here, instead of explicit calls to __init__
         super(D, self).__init__()
         print("<- D")
+
+    @tp.overload
+    def get_abstract_value1():
+        pass
 
 if __name__ == "__main__":
     D()
