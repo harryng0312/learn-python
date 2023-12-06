ae30ac6fd7f972db95d921d6388e4766ee19ada5
harryng0312 <little.ruan@gmail.com>
2023-12-07 00:49:31 +0700

    tkinter

--- module/tkinter/views.py
+++ module/tkinter/views.py
@@ -11,29 +11,43 @@ class PasswordFormView(ttk.Frame):
         return
 
     def __add_controls(self) -> None:
-        style = ttk.Style(master=self.master)
-        # style.theme_use("default")
+        self.master.grid_rowconfigure(index=0, weight=1)
+        self.master.grid_columnconfigure(index=0, weight=1)
+        style = ttk.Style(master=self)
+        style.theme_use("default")
+        # lb_style.theme_use("default")
         # Create style used by default for all Frames
-        style.configure(style='ThemedFrame.TFrame', padding=10)
+        style.configure(style='tf.TFrame', padding=(0, 5))
+        lb_style = ttk.Style(master=self).configure(style='tf.TLabel')
+        txt_style = ttk.Style(master=self).configure(style='tf.TEntry')
         # add frame
-        self.config(style="ThemedFrame.TFrame")
+        self.config(style="tf.TFrame")
+        # self.config(style="tf.TWidget")
         # create layout
-        self.grid(row=7, column=2, padx=10, pady=10)
+        self.grid(row=0, column=0, sticky="nsew")
+        self.grid_columnconfigure(index=1, weight=1)
+        # self.grid_configure(ipadx=15, ipady=15, sticky="nsew")
+        # self.pack(fill=tk.BOTH, expand=True)
         # frm.pack(expand=True)
         # add controls
-        lb_client_pub_key = ttk.Label(master=self, text="Client Public Key")
-        lb_client_pub_key_val = ttk.Label(master=self, text="")
-        lb_server_pub_key = ttk.Label(master=self, text="Server Public Key")
-        txt_server_pub_key = ScrolledText(master=self, width=36, height=4)
+        lb_client_pub_key = ttk.Label(master=self, style="tf.TLabel", text="Client Public Key")
+        lb_client_pub_key_val = ttk.Label(master=self, style="tf.TLabel", text="")
+        lb_server_pub_key = ttk.Label(master=self, style="tf.TLabel", text="Server Public Key")
+        # txt_server_pub_key = ScrolledText(master=self, width=36, height=4)
+        txt_server_pub_key = ttk.Entry(master=self, style="tf.TEntry")
+        txt_session_id = ttk.Entry(master=self, style="tf.TEntry")
+        
         # txt_server_pub_key.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
         btn_enc = ttk.Button(master=self, text="Click Me")
 
-        lb_client_pub_key.grid(row=0, column=0)
-        lb_client_pub_key_val.grid(row=0, column=1)
-        lb_server_pub_key.grid(row=1, column=0)
-        txt_server_pub_key.grid(row=1, column=1)
-        btn_enc.grid(row=2, column=1)
+        # layout
+        lb_client_pub_key.grid(row=0, column=0, padx=(0, 5), pady=(0, 5), sticky="e")
+        lb_client_pub_key_val.grid(row=0, column=1, padx=(0, 5), pady=(0, 5), sticky="ew")
+        lb_server_pub_key.grid(row=1, column=0, padx=(0, 5), pady=(0, 5), sticky="e")
+        txt_server_pub_key.grid(row=1, column=1, padx=(0, 5), pady=(0, 5), sticky="ew")
+        txt_session_id.grid(row=2, column=1, padx=(0, 5), pady=(0, 5), sticky="ew")
+        btn_enc.grid(row=5, column=1, columnspan=2, padx=(0, 5), pady=(0, 5), sticky="w")
         
         # self.pack()
         # bind events
