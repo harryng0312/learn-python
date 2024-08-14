1f00af5b5c09f31476b776e36a957d0ccc4f0ed5
harryng0312 <little.ruan@gmail.com>
2024-08-15 00:54:02 +0700

    try to create NN manually

--- module/ml/nn/mynn/losses.py
+++ module/ml/nn/mynn/losses.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def softmax(z_i: float, z: np.ndarray) -> float:
-    return np.exp(z_i) / np.sum(np.exp(z))
+def softmax(z: np.ndarray) -> float:
+    return np.exp(z) / np.sum(np.exp(z))
 
-def grad_softmax(z_i: float, z: np.ndarray):
+def grad_softmax(z: np.ndarray):
     # create S diagonal matrix with softmax(z_i, z)
     # create outer product matrix of softmax(z_i, z)
     # grad = S - outer_product(softmax, softmax.T)
-    return
+    sz = softmax(z)
+    return np.diag(sz) - np.outer(sz, sz)
